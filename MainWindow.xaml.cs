585c3a2666b77e6d5918482c2cf2bda6cfe79ba0
Michael Galetzka <galetzka@posteo.de>
2020-07-10 02:27:34 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using Fast_Jira.core;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fast_Jira
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Issue DisplayedIssue;        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Style = (Style)FindResource(typeof(Window));
+
+            ProgressBar.IsIndeterminate = false;
+
+            SetExampleIssue();
+            RefreshDisplayedIssue();
+        }
+
+        public void RefreshDisplayedIssue()
+        {
+            DetailsStatus.Text = DisplayedIssue?.Status?.Name;
+            DetailsPriority.Text = DisplayedIssue?.Priority?.Name;
+            DetailsResolution.Text = DisplayedIssue?.Resolution;
+            DetailsType.Text = DisplayedIssue?.Type?.Name;
+            DetailsAssignee.Text = DisplayedIssue?.Assignee?.DisplayName;
+            DetailsReporter.Text = DisplayedIssue?.Reporter?.DisplayName;
+            DetailsCreated.Text = DisplayedIssue?.Created == null ? "" : DisplayedIssue.Created.ToLocalTime().ToString();
+            DetailsUpdated.Text = DisplayedIssue?.Updated == null ? "" : DisplayedIssue.Updated.ToLocalTime().ToString();
+
+            SubtaskGroup.Visibility = DisplayedIssue?.Subtasks.Count > 0 ? Visibility.Visible : Visibility.Collapsed;
+            AttachmentsGroup.Visibility = DisplayedIssue?.Attachments.Count > 0 ? Visibility.Visible : Visibility.Collapsed;
+
+            SummaryText.Text = DisplayedIssue?.Summary == null ? "- issue summary missing -" : DisplayedIssue.Key + " - " + DisplayedIssue.Summary;
+            MarkdownViewer.Markdown = DisplayedIssue?.Description;
+        }
+
+        private void OpenHyperlink(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show($"URL: {e.Parameter}");
+        }
+
+        private void ClickOnImage(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show($"Image: {e.Parameter}");
+        }
+
+        private void SetExampleIssue()
+        {
+            DisplayedIssue = new Issue
+            {
+                ID = "123",
+                Key = "DXFAA-339",
+                Description = "Test it here:\r\n [Link](https://some-link/abc)\r\n\r\n**Some bold stuff!**",
+                Summary = "Test issue for all purposes :)",
+                Created = DateTime.Now,
+                Updated = DateTime.Now,
+                Status = new IssueStatus
+                {
+                    Name = "TO DO"
+                }
+            };
+        }
+    }
+
+    public class DetailEntry
+    {
+        public string Title { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ core/Issue.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fast_Jira.core
+{
+    public class Issue
+    {
+        /// <summary>Internal Jira ID</summary>
+        public string ID { get; set; }
+
+        /// <summary>Issue key as shown in the browser</summary>
+        public string Key { get; set; }
+
+        /// <summary>Issue description in markdown format</summary>
+        public string Description { get; set; }
+
+        /// <summary>Last known scrollbar position of the description text</summary>
+        public int DescriptionScrollPosition { get; set; }
+
+        /// <summary>Issue summary displayed on top as the heading</summary>
+        public string Summary { get; set; }
+
+        public string Resolution { get; set; }
+
+        public DateTime Updated { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public List<string> IssueLinks { get; } = new List<string>();
+
+        public List<string> Subtasks { get; } = new List<string>();
+
+        public List<Comment> Comments { get; } = new List<Comment>();
+
+        public List<Attachment> Attachments { get; } = new List<Attachment>();
+
+        public Person Assignee { get; set; }
+
+        public Person Reporter { get; set; }
+
+        public IssueType Type { get; set; }
+
+        public IssueType Priority { get; set; }
+
+        public IssueStatus Status { get; set; }
+
+        public Project Project { get; set; }
+
+        public Dictionary<string, string> CustomFields { get; } = new Dictionary<string, string>();
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public string Key { get; set; }
+
+        public string Email { get; set; }
+
+        // TODO: get icon
+        //public string AvatarUrl { get; set; }
+
+        public string DisplayName { get; set; }
+    }
+
+    public class IssueType
+    {
+        public string ID { get; set; }
+
+        public string Name { get; set; }
+
+        // TODO: get icon
+        //public string IconUrl { get; set; }
+    }
+
+    public class IssueStatus
+    {
+        public string ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Color { get; set; }
+    }
+
+    public class Project
+    {
+        public string ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Key { get; set; }
+
+        // TODO: get icon
+        //public string AvatarUrl { get; set; }
+    }
+
+    public class Comment
+    {
+        public string ID { get; set; }
+
+        public Person Author { get; set; }
+
+        public string Body { get; set; }
+
+        public DateTime Created { get; set; }
+    }
+
+    public class Attachment
+    {
+        public string ID { get; set; }
+
+        public Person Author { get; set; }
+
+        public string FileName { get; set; }
+
+        public string Content { get; set; }
+
+        public string Thumbnail { get; set; }
+
+        public int Size { get; set; }
+
+        public DateTime Created { get; set; }
+    }
+}
